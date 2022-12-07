71fee01afa8fefdde364cbcc7f79da3fcadb4a1d
karanidenis <d.waweru@alustudent.com>
2022-12-07 14:51:00 +0000

    puppet

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 
 
 exec {'new word':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php', 
+    command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php', 
     provider => shell,
 }
